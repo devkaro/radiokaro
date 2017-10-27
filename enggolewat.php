189ab1211822906ac5e5a8eb7b810acf9a1da96a
Tommy A. Surbakti <tommy@surbakti.net>
2017-10-27 12:43:33 +0700

    format filena

--- enggolewat.php
+++ enggolewat.php
@@ -1,4 +1,5 @@
 <?php
+
 function fungsiCurl($url){
      $data = curl_init();
      curl_setopt($data, CURLOPT_RETURNTRANSFER, 1);
@@ -12,24 +13,24 @@ function fungsiCurl($url){
 $linkUrl = fungsiCurl('https://bupati.karo.or.id:8080/played.html');
 
 // function sienggoLewat($linkUrl) {
-    $tabel = explode('<table border=0 cellpadding=2 cellspacing=2>', $linkUrl);
-    $akhirTabel = explode ('</table>',$tabel[0]);
-    preg_match_all("'<tr>(.*?)</tr>'si",$akhirTabel[1], $match);
-    echo '<table class="table table-hover table-striped">';
-    echo "<caption>Simbaru i putar</caption>
-    <thead>
-        <tr>
-            <th><b>Jam</b></th>
-            <th>Sirende - Judul Lagu</th>
-        </tr>
-    </thead>";
-    foreach($match[1] as $val => $el)
-    {
-        if($val !== 0 and $val !== 1) {
-        echo "<tr>" . $el."</tr>";
-        }
+$tabel = explode('<table border=0 cellpadding=2 cellspacing=2>', $linkUrl);
+$akhirTabel = explode ('</table>',$tabel[0]);
+preg_match_all("'<tr>(.*?)</tr>'si",$akhirTabel[1], $match);
+echo '<table class="table table-hover table-striped">';
+echo "<caption>Simbaru i putar</caption>
+<thead>
+    <tr>
+        <th><b>Jam</b></th>
+        <th>Sirende - Judul Lagu</th>
+    </tr>
+</thead>";
+foreach($match[1] as $val => $el)
+{
+    if($val !== 0 and $val !== 1) {
+    echo "<tr>" . $el."</tr>";
     }
-    echo "</table>";
+}
+echo "</table>";
 // }
 
 // echo sienggoLewat();
