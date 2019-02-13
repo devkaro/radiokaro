78180c71852ac6813359cfb13dfb54b9bb8402f4
Tommy A. Surbakti <tommy@surbakti.net>
2019-02-13 15:08:25 +0700

    ganti parsing arah shoutcast ku icecast

--- sangana.php
+++ sangana.php
@@ -1,19 +1,36 @@
 <?php
+/**
+* @author Tommy A. Surbakti <tommy@surbakti.com>
+**/
 
-function fungsiCurl($url){
-     $data = curl_init();
-     curl_setopt($data, CURLOPT_RETURNTRANSFER, 1);
-     curl_setopt($data, CURLOPT_URL, $url);
-     curl_setopt($data, CURLOPT_HEADER, false);
-	 curl_setopt($data, CURLOPT_USERAGENT, "Mozilla/5.0 (Windows; U; Windows NT 5.1; en-GB; rv:1.8.1.6) Gecko/20070725 Firefox/2.0.0.6");
-     $hasil = curl_exec($data);
-     curl_error($data);
-     curl_close($data);
-     return $hasil;
+function fungsiCurl($url)
+{
+    $data = curl_init();
+    curl_setopt($data, CURLOPT_RETURNTRANSFER, 1);
+    curl_setopt($data, CURLOPT_URL, $url);
+    curl_setopt($data, CURLOPT_USERAGENT, "Mozilla/5.0 (X11; Fedora; Linux x86_64; rv:65.0) Gecko/20100101 Firefox/65.0");
+    $hasil = curl_exec($data);
+    curl_close($data);
+    return $hasil;
 }
 
-$linkUrl= fungsiCurl('https://bupati.karo.or.id:8080/7.html');
-$sirende = explode(",",$linkUrl);
-echo strip_tags($sirende[6]);
+$url = fungsiCurl('http://git.ndikkar.com:8000/status-json.xsl');
 
-?>
+$data = json_decode($url, true);
+
+$dataRadio = [];
+
+if ($url) {
+    foreach ($data as $key => $value) {
+        if (! $value['source']) {
+            die('Server streaming die');
+        }
+        foreach ($value['source'] as $key => $value) {
+            $dataRadio[$key] = $value;
+        }
+    }
+} else {
+    echo "ada error";
+}
+
+echo $dataRadio['title'];
