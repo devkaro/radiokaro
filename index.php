658cf5c3b254e293a522ed56902ccc150c110e59
Tommy A. Surbakti <tommy@surbakti.net>
2019-02-13 15:12:31 +0700

    hapus curl arah index.php

--- index.php
+++ index.php
@@ -63,41 +63,14 @@
                 </div>
             </div>
             <div class="col-md-6">
-                <?php
-                     function bacaHTML($url){
-                     $data = curl_init();
-                     curl_setopt($data, CURLOPT_RETURNTRANSFER, 1);
-                     curl_setopt($data, CURLOPT_URL, $url);
-                     curl_setopt($data, CURLOPT_USERAGENT, "Mozilla/5.0 (Windows; U; Windows NT 5.1; en-GB; rv:1.8.1.6) Gecko/20070725 Firefox/2.0.0.6");
-                     $hasil = curl_exec($data);
-                     curl_close($data);
-                     return $hasil;
-                }
-                $urlshoutcast= bacaHTML('https://bupati.karo.or.id:8080/index.html?sid=1');
-                $pecah = explode('<table cellpadding="2" cellspacing="0" border="0" align="center" style="padding-left:1em;">', $urlshoutcast);
-                $akhirTabel = explode ('</table>', $pecah[1]);
-                preg_match_all("'<td>(.*?)</td>'si",$akhirTabel[0], $match);
-
-                $data = array();
-                foreach($match[0] as $el)
-                {
-                    $data[] = $el;
-                }
-                ?>
                 <table class="table table-hover">
                     <tr>
                         <td>Status Radio</td>
-                        <td><?php $status = $data[15];
-                        if ($status) {
-                            echo '<p class="label label-primary">Online</span>';
-                        } else {
-                            echo '<p class="label label-danger">Offline</span>';
-                        }
-                        ?></td>
+                        <td><p class="label label-primary">Online</p></td>
                     </tr>
                     <tr>
                         <td>Simegi-megi</td>
-                        <td><b><?php echo strip_tags($data[5]); ?></b> <small>kalak</small></td>
+                        <td><b></b> <small>kalak</small></td>
                     </tr>
                     <tr class="alert-info">
                         <td colspan="2">
